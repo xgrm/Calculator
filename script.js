779b9fdafe33bd641b8215f5e73175c8ebc43dae
saarmoseri <xgrm406@gmail.com>
2022-01-03 15:00:29 +0200

    the calc func

--- script.js
+++ script.js
@@ -29,3 +29,25 @@ const multi = (x) => {
     return z * y;
   };
 };
+
+const acts = { p: plus, m: minus, d: divid, mult: multi };
+const data = { act: null, number: null };
+
+function input(props) {
+  const output = document.getElementById("screen_output");
+  let current_value = output.value;
+
+  if (props === "DEL") {
+    data.number = output.value = current_value.slice(
+      0,
+      current_value.length - 1
+    );
+  } else if (props === "=") {
+    data.number = output.value = data.act(parseFloat(data.number)).toString(10);
+  } else if (acts[props] === undefined) {
+    data.number = output.value = current_value + props;
+  } else {
+    output.value = " ";
+    data.act = acts[props](parseFloat(data.number));
+  }
+}
